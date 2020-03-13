ca8d17eb97934d504e1dfa9b6c7fb46e6578bf30
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-13 17:24:04 +0100

    add boutton toggle

---
+++ js/toggle.js
@@ -0,0 +1,21 @@
+
+var button = document.getElementById('button-toggle')
+var contentButton = document.getElementById('content-button-toggle')
+var etat = false
+button.addEventListener('click', changeToggle)
+
+function changeToggle(){
+    etat = !etat
+    if(etat){
+        this.classList.add("anim-button-toggle-true")
+        this.classList.remove("anim-button-toggle-false")
+        setTimeout(() => {
+            contentButton.style.backgroundColor = "#96ed98"
+        }, 250)
+        
+    }else if(!etat){
+        this.classList.add("anim-button-toggle-false")
+        this.classList.remove("anim-button-toggle-true")
+        contentButton.style.backgroundColor = "#aeaeae"
+    }
+}
