da274c722bdc6f71a80cd413bfb998be0dca8a4c
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-25 10:31:09 +0100

    add anim rs

--- js/buttonNavBar.js
+++ js/buttonNavBar.js
@@ -1,6 +1,7 @@
 var button = document.getElementById('button-nav')
 var etat = false
 var nav = document.getElementById('cont-options')
+var rsContaine = document.getElementById('js-amine-rs')
 button.addEventListener('click', function(){
     etat = !etat
     if(etat){
@@ -9,6 +10,9 @@ button.addEventListener('click', function(){
 
         this.classList.remove("anim-nav-button-rigth")
         this.classList.add('anim-nav-button-left')
+
+        rsContaine.classList.remove('anim-rs-hidden')
+        rsContaine.classList.add('anim-rs-visible')
         
     }else if(!etat){
         nav.classList.remove('anim-nav-visible')
@@ -16,5 +20,8 @@ button.addEventListener('click', function(){
 
         this.classList.remove('anim-nav-button-left')
         this.classList.add("anim-nav-button-rigth")
+
+        //rsContaine.classList.remove('anim-rs-visible')
+        rsContaine.classList.add('anim-rs-hidden')
     }
 }, false)
