7f911b5845528ac7134f15432e2dc5023a2d412d
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-26 00:50:24 +0100

    corection bug du bambou

--- js/my.js
+++ js/my.js
@@ -4,12 +4,17 @@ var bambou = document.getElementById('js-my-bambou')
 toggle.addEventListener('click', function() {
     if(etat == true){
         containerNotreAgence.classList.add('change-notre-agence')
+
         colorTextNotreAgence.classList.add('change-color-h3')
+
         bambou.classList.remove('anim-bambou-hidden')
         bambou.classList.add('anim-bambou-visible')
+        
     }else if(etat == false){
         containerNotreAgence.classList.remove('change-notre-agence')
+
         colorTextNotreAgence.classList.remove('change-color-h3')
+
         bambou.classList.remove('anim-bambou-visible')
         bambou.classList.add('anim-bambou-hidden')
     }
