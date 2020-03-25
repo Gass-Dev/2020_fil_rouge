676e6e1bec08552d56ba6d5508a0309920f413fb
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-25 14:22:17 +0100

    modif en savoir +

--- js/buttonNavBar.js
+++ js/buttonNavBar.js
@@ -1,10 +1,10 @@
 var button = document.getElementById('button-nav')
-var etat = false
+var etat2 = false
 var nav = document.getElementById('cont-options')
 var rsContaine = document.getElementById('js-amine-rs')
 button.addEventListener('click', function(){
-    etat = !etat
-    if(etat){
+    etat2 = !etat2
+    if(etat2){
         nav.classList.remove('anim-nav-hidden')
         nav.classList.add('anim-nav-visible')
 
@@ -14,14 +14,14 @@ button.addEventListener('click', function(){
         rsContaine.classList.remove('anim-rs-hidden')
         rsContaine.classList.add('anim-rs-visible')
         
-    }else if(!etat){
+    }else if(!etat2){
         nav.classList.remove('anim-nav-visible')
         nav.classList.add('anim-nav-hidden')
 
         this.classList.remove('anim-nav-button-left')
         this.classList.add("anim-nav-button-rigth")
 
-        //rsContaine.classList.remove('anim-rs-visible')
+        rsContaine.classList.remove('anim-rs-visible')
         rsContaine.classList.add('anim-rs-hidden')
     }
 }, false)
