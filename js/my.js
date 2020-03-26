f972d9429c342c345ee180df3c0e8583f04b813a
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-27 00:52:42 +0100

    finnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- js/my.js
+++ js/my.js
@@ -3,7 +3,8 @@ var colorTextNotreAgence = document.getElementById('js-my-title-agence')
 var bambou = document.getElementById('js-my-bambou')
 var imgOrdi = document.getElementById('js-my-img-na')
 var darkPanda = document.getElementById('js-my-dark-panda')
-
+var bgCwac = document.getElementById('js-my-bg-cwac')
+var teams = document.getElementById('teams')
 toggle.addEventListener('click', function() {
     if(etat == true){
         containerNotreAgence.classList.add('change-notre-agence')
@@ -17,6 +18,9 @@ toggle.addEventListener('click', function() {
         darkPanda.classList.add('dark-panda-visible')
         imgOrdi.classList.remove('ordi-visible')
         imgOrdi.classList.add('ordi-hidden')
+
+        bgCwac.classList.add('cwac')
+        teams.classList.add('team-s')
         
     }else if(etat == false){
         containerNotreAgence.classList.remove('change-notre-agence')
@@ -30,5 +34,8 @@ toggle.addEventListener('click', function() {
         darkPanda.classList.add('dark-panda-hidden')
         imgOrdi.classList.remove('ordi-hidden')
         imgOrdi.classList.add('ordi-visible')
+
+        bgCwac.classList.remove('cwac')
+        teams.classList.remove('team-s')
     }
 }) 
