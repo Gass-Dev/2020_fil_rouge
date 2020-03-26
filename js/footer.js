37321b29f1cbdfa69e070825cecf43e1c64ca429
aminehadefdev <amine.hadef.dev@gmail.com>
2020-03-26 21:13:16 +0100

    add responsiv footer and change logo footer to toggle

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,17 @@
 toggle.addEventListener('click', function(){
     var element = document.getElementById('footer')
+    var logoFooter = document.getElementById('LogoPandaWeb')
     if(etat == true){
         //element.style.backgroundColor = 'linear-gradient(180deg, #BEF164, #6CBB4D)'
         element.classList.remove('change-background-footer1')
         element.classList.add('change-background-footer2')
+
+        logoFooter.classList.add('footer-logo-change-toggle')
     }else if(etat == false){
         //element.style.backgroundColor = '#090D0F'
         element.classList.remove('change-background-footer2')
         element.classList.add('change-background-footer1')
+
+        logoFooter.classList.remove('footer-logo-change-toggle')
     }
 })
